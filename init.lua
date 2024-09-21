d229d26aff0405a77021605df6fd3845f02c7792
Horace Liu <im.liuhq@gmail.com>
2024-09-22 00:03:05 +0800

    chore: nothing

--- init.lua
+++ init.lua
@@ -9,12 +9,10 @@ vim.g.loaded_netrwPlugin = 1
 
 if not vim.g.vscode then
     -- ordinary Neovim
-    -- Config --
     require('config/options')
     require('config/autocmds')
     require('config/keymaps')
     require('config/lazy')
-    -- Colorscheme --
     vim.cmd.colorscheme('catppuccin')
 else
     -- VSCode extension
