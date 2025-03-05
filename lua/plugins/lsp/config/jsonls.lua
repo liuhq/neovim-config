2dbae0170152f5be2fbcdfef60a3058380dc08cd
Hitorin <im.liuhq@gmail.com>
2025-03-05 22:18:11 +0800

    feat(jsonls): json schemas support

--- lua/plugins/lsp/config/jsonls.lua
+++ lua/plugins/lsp/config/jsonls.lua
@@ -23,8 +23,59 @@ function M.setup(on_attach_base, handlers)
             client.capabilities.textDocument.completion.completionItem.snippetSupport = true
         end,
         handlers = handlers,
+        settings = {
+            json = {
+                schemas = {
+                    {
+                        fileMatch = { 'package.json' },
+                        url = 'https://json.schemastore.org/package.json',
+                    },
+                    {
+                        fileMatch = { 'tsconfig*.json' },
+                        url = 'https://json.schemastore.org/tsconfig.json',
+                    },
+                    {
+                        fileMatch = {
+                            '.prettierrc',
+                            '.prettierrc.json',
+                            'prettier.config.json',
+                        },
+                        url = 'https://json.schemastore.org/prettierrc.json',
+                    },
+                    {
+                        fileMatch = { '.eslintrc', '.eslintrc.json' },
+                        url = 'https://json.schemastore.org/eslintrc.json',
+                    },
+                },
+            },
+        },
     }
     lspconfig['jsonls'].setup(opts)
 end
 
 return M
+
+
+
+--- note: yamlls settings
+-- yamlls = {
+--     settings = {
+--         yaml = {
+--             -- Schemas https://www.schemastore.org
+--             schemas = {
+--                 ['http://json.schemastore.org/gitlab-ci.json'] = { '.gitlab-ci.yml' },
+--                 ['https://json.schemastore.org/bamboo-spec.json'] = {
+--                     'bamboo-specs/*.{yml,yaml}',
+--                 },
+--                 ['https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json'] = {
+--                     'docker-compose*.{yml,yaml}',
+--                 },
+--                 ['http://json.schemastore.org/github-workflow.json'] = '.github/workflows/*.{yml,yaml}',
+--                 ['http://json.schemastore.org/github-action.json'] = '.github/action.{yml,yaml}',
+--                 ['http://json.schemastore.org/prettierrc.json'] = '.prettierrc.{yml,yaml}',
+--                 ['http://json.schemastore.org/stylelintrc.json'] = '.stylelintrc.{yml,yaml}',
+--                 ['http://json.schemastore.org/circleciconfig'] = '.circleci/**/*.{yml,yaml}',
+--             },
+--         },
+--     },
+-- },
