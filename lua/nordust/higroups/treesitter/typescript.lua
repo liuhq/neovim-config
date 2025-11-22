7fda7aedde8ef2f9893d404068c6d68a1ac21383
Horace Liu <im.liuhq@gmail.com>
2025-11-22 20:46:51 +0800

    chore: reset @@markup.link.label style in tsx

--- lua/nordust/higroups/treesitter/typescript.lua
+++ lua/nordust/higroups/treesitter/typescript.lua
@@ -4,8 +4,12 @@ local C = require('nordust.colors')
 local hl = require('nordust.utils').hl
 
 M.get = function ()
+    hl('@variable.member.private.javascript', '', '', { italic = true })
     hl('@variable.member.private.typescript', '', '', { italic = true })
-    hl('@variable.member.private.typescriptreact', { link = '@variable.member.private.typescript' })
+    hl('@variable.member.private.tsx', { link = '@variable.member.private.typescript' })
+
+    hl('@markup.link.label.javascript', C.aurora_green, '', { underline = false })
+    hl('@markup.link.label.tsx', C.aurora_green, '', { underline = false })
 end
 
 return M
