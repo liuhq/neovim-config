a9f4ce9089b775eb49998acfa0b09aef440b8964
Horace Liu <im.liuhq@gmail.com>
2025-11-23 17:03:41 +0800

    chore: update colorscheme

--- queries/bash/highlights.scm
+++ queries/bash/highlights.scm
@@ -6,14 +6,14 @@
 
 (command
   argument: (_) @variable.parameter.path (#set! priority 105)
-  (#match? @variable.parameter.path "^\\S*/(($\\w+|$\\{\\w+\\}|\\w+)(/($\\w+|$\\{\\w+\\}|\\w+))*)\\S*$"))
+  (#match? @variable.parameter.path "^(\\./|\\.\\./|~|/)(($\\w+|$\\{\\w+\\}|\\w+)(/($\\w+|$\\{\\w+\\}|\\w+))*)\\S*$"))
 
 ((string_content) @variable.parameter.path (#set! priority 105)
-  (#match? @variable.parameter.path "^\\S*/(($\\w+|$\\{\\w+\\}|\\w+)(/($\\w+|$\\{\\w+\\}|\\w+))*)\\S*$"))
+  (#match? @variable.parameter.path "^(\\./|\\.\\./|~|/)(($\\w+|$\\{\\w+\\}|\\w+)(/($\\w+|$\\{\\w+\\}|\\w+))*)\\S*$"))
 
 (variable_assignment
   value: (_) @variable.parameter.path (#set! priority 105)
-  (#match? @variable.parameter.path "^\\S*/(($\\w+|$\\{\\w+\\}|\\w+)(/($\\w+|$\\{\\w+\\}|\\w+))*)\\S*$"))
+  (#match? @variable.parameter.path "^(\\./|\\.\\./|~|/)(($\\w+|$\\{\\w+\\}|\\w+)(/($\\w+|$\\{\\w+\\}|\\w+))*)\\S*$"))
 
 ((simple_expansion) @variable (#set! priority 105))
 ((expansion) @variable (#set! priority 105))
