6d95915fe5740a2c6c655bcaf770c7267208293a
Horace Liu <im.liuhq@gmail.com>
2025-12-17 16:17:26 +0800

    fix(query): shell parse error

--- queries/bash/highlights.scm
+++ queries/bash/highlights.scm
@@ -4,16 +4,16 @@
   argument: (_) @variable.parameter.followCommand
   (#match? @variable.parameter.followCommand "^(-|--|\\+).*"))
 
-(command
-  argument: (_) @variable.parameter.path (#set! priority 105)
-  (#match? @variable.parameter.path "^(\\./|\\.\\./|~|/)(($\\w+|$\\{\\w+\\}|\\w+)(/($\\w+|$\\{\\w+\\}|\\w+))*)\\S*$"))
-
-((string_content) @variable.parameter.path (#set! priority 105)
-  (#match? @variable.parameter.path "^(\\./|\\.\\./|~|/)(($\\w+|$\\{\\w+\\}|\\w+)(/($\\w+|$\\{\\w+\\}|\\w+))*)\\S*$"))
-
-(variable_assignment
-  value: (_) @variable.parameter.path (#set! priority 105)
-  (#match? @variable.parameter.path "^(\\./|\\.\\./|~|/)(($\\w+|$\\{\\w+\\}|\\w+)(/($\\w+|$\\{\\w+\\}|\\w+))*)\\S*$"))
+; (command
+;   argument: (_) @variable.parameter.path (#set! priority 105)
+;   (#match? @variable.parameter.path "^(\\./|\\.\\./|~|/)(($\\w+|$\\{\\w+\\}|\\w+)(/($\\w+|$\\{\\w+\\}|\\w+))*)\\S*$"))
+;
+; ((string_content) @variable.parameter.path (#set! priority 105)
+;   (#match? @variable.parameter.path "^(\\./|\\.\\./|~|/)(($\\w+|$\\{\\w+\\}|\\w+)(/($\\w+|$\\{\\w+\\}|\\w+))*)\\S*$"))
+;
+; (variable_assignment
+;   value: (_) @variable.parameter.path (#set! priority 105)
+;   (#match? @variable.parameter.path "^(\\./|\\.\\./|~|/)(($\\w+|$\\{\\w+\\}|\\w+)(/($\\w+|$\\{\\w+\\}|\\w+))*)\\S*$"))
 
 ((simple_expansion) @variable (#set! priority 105))
 ((expansion) @variable (#set! priority 105))
