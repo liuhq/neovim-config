fe9504d2251ff6ae7a5fc884d1f1219dab164c1f
Horace Liu <im.liuhq@gmail.com>
2026-08-09 19:44:59 +0800

    fix: pack add

--- plugin/10-blink-cmp.lua
+++ plugin/10-blink-cmp.lua
@@ -1,4 +1,9 @@
-vim.pack.add({ src = 'https://github.com/saghen/blink.cmp', version = vim.version.range('1.*') })
+vim.pack.add({
+  {
+    src = 'https://github.com/saghen/blink.cmp',
+    version = vim.version.range('1.*'),
+  },
+})
 
 local snippets_dirs = function()
   local dirs = {}
